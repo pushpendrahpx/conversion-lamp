dd64d3ef3b6ac849fb40cdc15b34d629e3eef196
Shivam Chourasia <shivamchourasia15@gmail.com>
2021-10-10 13:13:49 +0530

    moved inline css to separate css files.

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -1,16 +1,21 @@
-import React from 'react'
-import { Menu, Layout,Breadcrumb } from 'antd';
-import { MailOutlined, AppstoreOutlined, SettingOutlined } from '@ant-design/icons';
+import React from "react";
+import "./Navbar.css";
+import { Menu, Layout, Breadcrumb } from "antd";
+import {
+  MailOutlined,
+  AppstoreOutlined,
+  SettingOutlined,
+} from "@ant-design/icons";
 const { Header, Content, Footer } = Layout;
 const { SubMenu } = Menu;
 
 class Navbar extends React.Component {
   state = {
-    current: 'mail',
+    current: "mail",
   };
 
-  handleClick = e => {
-    console.log('click ', e);
+  handleClick = (e) => {
+    console.log("click ", e);
     this.setState({ current: e.key });
   };
 
@@ -18,26 +23,32 @@ class Navbar extends React.Component {
     const { current } = this.state;
     return (
       <Layout>
-    <Header style={{ position: 'fixed', zIndex: 1, width: '100%', display:"inline-flex" }}>
-      <a href="/">
-        <div className="logo" style={{color:"white", fontSize:"24px",cursor:"pointer",userSelect:"none"}}>
-          ConversionLamp
-        </div>
-      </a>
+        <Header className="header-cover">
+          <a href="/">
+            <div className="logo">ConversionLamp</div>
+          </a>
 
-      <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['2']} style={{float:"right",placeContent:"end",width:"100%"}}>
-        <Menu.Item key="1">Blogs</Menu.Item>
-        <Menu.Item key="2"> <a href="https://github.com/pushpendrahpx/conversion-lamp">Github</a></Menu.Item>
-        <Menu.Item key="3">Sponsor</Menu.Item>
-        <Menu.Item key="4"> About </Menu.Item>
-      </Menu>
-    </Header>
-    <Content className="site-layout" style={{ padding: '0 50px', marginTop: 64 }}>
-      
-    </Content>
-  </Layout>
+          <Menu
+            theme="dark"
+            mode="horizontal"
+            defaultSelectedKeys={["2"]}
+            className="menu-cover"
+          >
+            <Menu.Item key="1">Blogs</Menu.Item>
+            <Menu.Item key="2">
+              {" "}
+              <a href="https://github.com/pushpendrahpx/conversion-lamp">
+                Github
+              </a>
+            </Menu.Item>
+            <Menu.Item key="3">Sponsor</Menu.Item>
+            <Menu.Item key="4"> About </Menu.Item>
+          </Menu>
+        </Header>
+        <Content className="site-layout"></Content>
+      </Layout>
     );
   }
 }
 
-export default Navbar
+export default Navbar;
